6d7f2b68b4341633ee9c718c5e7756af142e5f50
Julian Godesa <julian.godesa@googlemail.com>
2018-04-18 08:17:12 +0200

    so much for a small fix :-)

--- checker.go
+++ checker.go
@@ -11,11 +11,12 @@ type Checker struct {
 	Clients         *[]Client
 	HTTPClient      *http.Client
 	PollingInterval int
-	mu              sync.Mutex
+	mu              *sync.Mutex
 }
 
 func (c *Checker) StartPolling() {
 
+	c.mu = &sync.Mutex{}
 	pollingInterval := time.Duration(c.PollingInterval) * time.Second
 	tick := time.Tick(pollingInterval)
 	for {
@@ -29,22 +30,23 @@ func (c *Checker) StartPolling() {
 	}
 }
 
-func checkClient(mu sync.Mutex, webClient *http.Client, service *Client) {
+func checkClient(mu *sync.Mutex, webClient *http.Client, service *Client) {
 	resp, err := webClient.Get(service.URL)
 	if err != nil {
 		log.Printf("failed fetching '%s'", service.URL)
 		return
 	}
+	online := false
 
-	mu.Lock()
 	if resp.StatusCode < 200 || resp.StatusCode > 399 {
 		log.Printf("'%s' is down!", service.URL)
-		service.Online = false
-		return
+		online = false
+	} else {
+		log.Printf("'%s' is up!", service.URL)
+		online = true
 	}
 
-	// still online or recover case
-	log.Printf("'%s' is up!", service.URL)
-	service.Online = true
+	mu.Lock()
+	service.Online = online
 	mu.Unlock()
 }
